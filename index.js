fd4ef78caab5df2f642f845a16fe547695027db9
Pratik Var <npc.prat@gmail.com>
2020-10-24 14:47:47 +0530

    Complete mobile version

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ function nav() {
         isOpen = false;
     }
 }
+
+const acc = document.getElementsByClassName('accordion');
+for(let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function() {
+
+        let icon = acc[i].childNodes[1];
+        if(icon.innerHTML == 'expand_less') icon.innerHTML = 'expand_more';
+        else icon.innerHTML = 'expand_less';
+
+        let panel = this.nextElementSibling;
+        
+        if(panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            panel.style.marginTop = '0';
+        }
+        else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+            panel.style.marginTop = '16px';
+        }
+    });
+}
